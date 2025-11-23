61cd73ec6fed93d03671a77cfe5d90bf6a05ed90
aniqatasnia <78773073+aniqatasnia@users.noreply.github.com>
2025-11-23 13:46:02 -0800

    Update reverse-string.py
    Commented out less memory-optimal methods for reversing a string.

--- reverse-string.py
+++ reverse-string.py
@@ -8,3 +8,24 @@ class Solution:
             s[l],s[r] = s[r], s[l]
             l, r = (l + 1), (r - 1)
         return
+# less memory-optimal stack method: Space: O(n)
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         # Time: O(n) Space: O(n)
+#         stack = []
+#         for c in s:
+#             stack.append(c)
+#         i = 0
+#         while stack:
+#             s[i] = stack.pop()
+#             i += 1
+#         return
+
+# recursion (also less memory-optimal): Space: O(n)
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         def reverse(l, r):
+#             if l < r:
+#                 s[l], s[r] = s[r], s[l]
+#                 reverse(l + 1, r - 1)
+#         reverse(0, len(s) - 1)
