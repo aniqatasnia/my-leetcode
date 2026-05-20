f4b490c0afbea3eeb1c82efcb32e1e0e4bd2f2ac
aniqatasnia <78773073+aniqatasnia@users.noreply.github.com>
2026-05-20 16:02:33 -0700

    Implement second solution for first unique character
    Added a second solution for finding the first unique character using a different approach with dictionaries.

---
+++ easy/first-unique-character-in-a-string.py
@@ -0,0 +1,37 @@
+# Solution 1
+class Solution1:
+    def firstUniqChar(self, s: str) -> int:
+        count = defaultdict(int)
+
+        for c in s:
+            count[c] += 1
+        
+        for i, c in enumerate(s):
+            if count[c] == 1:
+                return i
+        
+        return -1
+# I feel like there's another way to do it, though...like with a seen queue and/or two pointers...
+# It was listed under queues in the Roblox problems list and I can't tell if this implements that queue solution or not.
+
+# Solution 2
+class Solution2:
+    def firstUniqChar(self, s: str) -> int:
+        seen = {}
+        unique = {}
+        for c in s:
+            if c not in seen:
+                unique[c] = c
+                seen[c] = c
+            elif c in unique:
+                unique.pop(c)
+
+        for i, c in enumerate(s):
+            if c in unique:
+                
+                return i
+        
+        return -1
+    # Okay there was another way to do it (with a seen queue at least, but haven't tried pointers yet) but it was effectively the same as the hashmap solution.
+    # Wait unless a queue has to be implemented as FIFO, I forgot shoot. 
+    # But even then how would you look up/would looking up allow it to be as efficient or more efficient than dictionary hashmap solution?
